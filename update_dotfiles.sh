65d29baf3c6441540fdada28560fdcf978ecf81d
sirhowitzer <aragosarus@tuta.io>
2026-04-20 04:56:37 +0700

    Update Dotfiles 20/04/2026

---
+++ update_dotfiles.sh
@@ -0,0 +1,17 @@
+mkdir -p ~/.dotfiles/.config/fuzzel/
+cp -r ~/.config/fuzzel/* ~/.dotfiles/.config/fuzzel/
+
+mkdir -p ~/.dotfiles/.config/kitty/
+cp -r ~/.config/kitty/* ~/.dotfiles/.config/kitty/
+
+mkdir -p ~/.dotfiles/.config/niri/
+cp -r ~/.config/niri/* ~/.dotfiles/.config/niri/
+
+mkdir -p ~/.dotfiles/.config/swaylock/
+cp -r ~/.config/swaylock/* ~/.dotfiles/.config/swaylock/
+
+mkdir -p ~/.dotfiles/.config/xplr/
+cp -r ~/.config/xplr/* ~/.dotfiles/.config/xplr/
+
+mkdir -p ~/.dotfiles/.config/alacritty/
+cp -r ~/.config/alacritty/* ~/.dotfiles/.config/alacritty/
